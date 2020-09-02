0609d79b2dba2f622dda48eb434762a68ef9cf2f
Glitch (jasper-quiver-chard) <none>
2020-09-02 11:55:22 +0000

    🎌🐇 Checkpoint ./server.js:21184821/2964

--- server.js
+++ server.js
@@ -1,51 +1,66 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser')
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 
-var cors = require('cors');
+var cors = require("cors");
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
+
 // mongoose.connect(process.env.DB_URI);
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/api/shorturl/new',(req,res)=>{
-  var url = req.body.url 
-  
+app.post("/api/shorturl/new", (req, res) => {
+  var postUrl = req.body.url;
+
   const urlSchema = new mongoose.Schema({
-      url : String,
-      place : Number
-  })
-  
-  
-})
+    url: String,
+    place: Number
+  });
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+  const Url = mongoose.model("Url", urlSchema);
+
+  let placeNumber = 1;
+
+  let ffc = new Url({ url: postUrl, place: placeNumber });
+
+  ffc.save((err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.listen(port, function() {
+  console.log("Node.js listening ...");
 });
